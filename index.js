21521052f93680ff901c2670b79903727130698b
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-27 04:51:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function superbowlWin(record) {
+    function superbowlWin(records) {
+        const winningRecord = records.find(record => record.result === "W");
+        if (win){
+            return win.year;
+            }
+            else{
+            return undefined;   
+            }
+      }
+    console.log (superbowlWin(records))
+    
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        { year: "2012", result: "N/A"},
+    ]
+}
