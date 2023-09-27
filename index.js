553f0930eceb92511827cd3e449cf47333d5a92b
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-27 05:08:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // code your solution here
-function superbowlWin(record) {
-    function superbowlWin(records) {
-        const winningRecord = records.find(record => record.result === "W");
-        if (win){
-            return win.year;
-            }
-            else{
-            return undefined;   
-            }
-      }
-    console.log (superbowlWin(records))
-    
-    const record = [
-        { year: "2015", result: "W"},
-        { year: "2014", result: "N/A"},
-        { year: "2013", result: "L"},
-        { year: "2012", result: "N/A"},
-    ]
-}
+function superbowlWin(records) {
+    const winningRecord = records.find(function(record) {
+        return (record.result === "W");
+    });
+
+    if (winningRecord) {
+        return winningRecord.year;
+    }
+    else{
+        return undefined;   
+    }
+} 
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+]
